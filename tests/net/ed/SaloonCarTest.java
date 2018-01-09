c45925537ebee811bf7a5c5627562fd7a49aae07
Edward Dupre <edwd42@gmail.com>
2018-01-08 22:48:46 -0500

    implement test for SaloonCar getColour

--- tests/net/ed/SaloonCarTest.java
+++ tests/net/ed/SaloonCarTest.java
@@ -9,16 +9,23 @@ public class SaloonCarTest {
     SaloonCar saloonCar = new SaloonCar(1, "Red", "Toyota");
 
     @Test
-    public void getNumberOfSeats() {
+    public void getNumberOfSeats() throws Exception{
         int expected = 11;
         int actual = saloonCar.getNumberOfSeats();
         assertEquals(expected, actual);
     }
 
     @Test
-    public void getManufacturer() {
+    public void getManufacturer() throws Exception{
         String expected = "Toyota";
         String actual = saloonCar.getManufacturer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getColour() throws Exception{
+        String expected = "Red";
+        String actual = saloonCar.getColour();
         assertEquals(expected, actual);
     }
 }
